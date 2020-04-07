28ecae3674e1f9c0688f7084168be658705f1602
HuydDo <huydinhdo@yahoo.com>
2020-04-07 13:37:16 -0600

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -39,13 +39,4 @@ class BandInput extends Component {
   }
 }
 
-// const mapDispatchToProps = dispatch => {
-//   return ({
-//     addBand: formData => dispatch({
-//       type:'ADD_BAND',
-//       payload: formData})
-//   })
-// }
-
-// export default connect(null, mapDispatchToProps)(BandInput);
 export default BandInput
