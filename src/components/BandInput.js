d649b187ec931ad34cfa15227d8d0bc360e76769
scrabill <shannon.crabill@gmail.com>
2020-04-08 15:10:34 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,26 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  // constructor() {
+  //   super(),
+  //
+  //   state = {
+  //     text: ''
+  //   }
+  // }
+
+  // state = {
+  //   text: ''
+  // }
+
+
+
   render() {
     return(
       <div>
         Band Input
+        <input type="text" name={this.state.text} />
       </div>
     )
   }
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
+import createStore from 'redux'
 
 class BandsContainer extends Component {
+
+  const store = createStore()
+
+  state = {
+    text: ''
+  }
+
   render() {
     return(
       <div>
         BandsContainer
+        <BandInput />
       </div>
     )
   }
